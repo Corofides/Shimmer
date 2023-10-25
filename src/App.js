dcd7e589f653f6adbec2d5581b1b415f2ef4c6b7
Corofides <J.l.lendrem@smoothlines.net>
2023-10-25 19:28:53 +0100

    attempt markdown post.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Markdown from 'react-markdown'
 import './App.css';
 
 function App() {
+
+  const [markDown, setMarkDown] = useState(false);
+
+  useEffect(() => {
+
+    fetch('/Shimmer/posts/2023-10-25_First%20Post.md').then(response => response.text()).then((text) => {
+      setMarkDown(text);
+    })
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Post">
+      <Markdown>{markDown}</Markdown>
     </div>
   );
 }
