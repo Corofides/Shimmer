faa487f2e04ffbf7e09dd9ab0e11ab403761d64d
Corofides <J.l.lendrem@smoothlines.net>
2023-10-26 20:20:57 +0100

    Add name to footer.

--- src/SiteFooter/SiteFooter.js
+++ src/SiteFooter/SiteFooter.js
@@ -6,6 +6,7 @@ export default () => {
   return (
     <footer className={"Footer"}>
       <div className={"Content"}>
+        <a href={"#"}>Corofides</a>
         <a href={"#"}>Information</a>
       </div>
     </footer>
