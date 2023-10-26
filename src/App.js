589742197b84f12b42b8da45a9cd26a8cff357b9
Corofides <J.l.lendrem@smoothlines.net>
2023-10-26 12:53:46 +0100

    add in json file for post data display list of posts from json file add some basic styling and colours use scss for styles instead of css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import React, {useEffect, useState} from 'react';
 import Markdown from 'react-markdown'
-import './App.css';
+import './App.scss';
 
 function App() {
 
   const [markDown, setMarkDown] = useState(false);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
 
+    fetch('/Shimmer/posts/posts.json').then(response => response.json()).then((result) => {
+      setPosts(result);
+    });
+
     fetch('/Shimmer/posts/2023-10-25_First%20Post.md').then(response => response.text()).then((text) => {
       setMarkDown(text);
-    })
+    });
+
+    fetch('/Shimmer/posts').then(response => response.text()).then((text) => {
+      console.log("Directory", text);
+    });
 
   }, []);
 
   return (
     <div className="Post">
+      {posts.map(({name, author, date_published}) => {
+        return (
+          <div className={"PostPreview"}>
+            <span>{date_published}</span>
+            <h2>{name}</h2>
+            <h3>{author}</h3>
+          </div>
+        )
+      })}
       <Markdown>{markDown}</Markdown>
     </div>
   );
