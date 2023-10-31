e31467988af725242917eebd74f93245e3a51493
Corofides <J.l.lendrem@smoothlines.net>
2023-10-31 07:59:47 +0000

    Add some styling for the author and title in Post Preview.

--- src/PostPreview/PostPreview.js
+++ src/PostPreview/PostPreview.js
@@ -8,9 +8,9 @@ export default ({onClick, id, name, author, date_published}) => {
     <div onClick={() => {onClick(id)}} className={"PostPreview"}>
       <div className={"PostPreview__Image"} />
       <div className={"PostPreview__Info"}>
-        <span>{date_published}</span>
-        <h2>{name}</h2>
-        <h3>{author}</h3>
+        <span className={"PostPreview__Published"}>{date_published}</span>
+        <h2 className={"PostPreview__Title"}>{name}</h2>
+        <h3 className={"PostPreview__Author"}>By {author}</h3>
       </div>
     </div>
   )
