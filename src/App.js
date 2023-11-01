4fa816913626c2ff896c89e0b564f23af5e7a7aa
Corofides <J.l.lendrem@smoothlines.net>
2023-11-01 21:46:08 +0000

    Remove redundant code.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import PostPreview from './PostPreview';
+import React from 'react';
 import Page from './Page';
-import Post from './Post';
-import usePosts from './Hooks/usePosts';
 import usePages from './Hooks/usePages';
 import Widgets from './Widgets';
 import './App.scss';
 
 function App() {
 
-  const {posts} = usePosts();
-  const {pages, loading, getCurrentPage} = usePages();
-
-  const [displayType, setDisplayType] = useState('home');
-  const [activeId, setActiveId] = useState(false);
+  const {loading, getCurrentPage} = usePages();
 
   if (loading) {
     return (
@@ -23,45 +16,20 @@ function App() {
 
   const currentPage = getCurrentPage();
 
-  console.log("Pages", pages);
-
-  if (true || pages.length > 0) {
-
-    return (
-      <div>
-        <Page>
-          <div className={"BlockPage"}>
-            {currentPage.widgets.map(({name, children, ...other}) => {
-
-              return (
-                <Widgets Cmp={name} {...other}>{children}</Widgets>
-              )
-
-            })}
-          </div>
-        </Page>
-      </div>
-    )
-
-  }
-
   return (
     <div>
       <Page>
         <div className={"BlockPage"}>
-          {posts.map(({id}) => {
+          {currentPage.widgets.map(({name, children, ...other}) => {
             return (
-              <PostPreview key={id} onClick={(id) => {
-
-                window.location.href = "/Shimmer#post/" + id;
-
-              }} id={id} />
+              <Widgets Cmp={name} {...other}>{children}</Widgets>
             )
           })}
         </div>
       </Page>
     </div>
-  );
+  )
+
 }
 
 export default App;
