a1feba7154c81ea97e3c14e7e6ed32f97d4d95af
Corofides <J.l.lendrem@smoothlines.net>
2023-11-01 20:49:39 +0000

    Add url matching - need to revisit this as I'm not sold on the way this works at current.

--- src/App.js
+++ src/App.js
@@ -10,60 +10,28 @@ import './App.scss';
 function App() {
 
   const {posts} = usePosts();
-  const {pages} = usePages();
+  const {pages, loading, getCurrentPage} = usePages();
 
   const [displayType, setDisplayType] = useState('home');
   const [activeId, setActiveId] = useState(false);
 
-  console.log("Pages", pages);
-
-  useEffect(() => {
-
-    console.log("Path", window.location.pathname);
-
-    const hasPathHash = window.location.href.split('#').length === 2;
-    let pathParts = window.location.pathname.split('/');
-
-    if (hasPathHash) {
-      pathParts = window.location.href.split('#')[1].split('/');
-      pathParts.unshift("Shimmer");
-      pathParts.unshift("");
-    }
-
-
-    if (pathParts.length > 2) {
-
-      if (pathParts.length >= 3) {
-        setDisplayType(pathParts[2]);
-      }
-
-      if (pathParts.length >= 4) {
-        setActiveId(pathParts[pathParts.length - 1]);
-      }
-
-    }
-
-  }, []);
-
-  console.log("ActiveId", activeId, posts[activeId]);
-
-  if (displayType === "post" && activeId >= 0 && activeId < posts.length) {
+  if (loading) {
     return (
-      <div>
-        <Page>
-          <Post post={posts[activeId]} />
-        </Page>
-      </div>
+      <div>Loading</div>
     )
   }
 
-  if (pages.length > 0) {
+  const currentPage = getCurrentPage();
+
+  console.log("Pages", pages);
+
+  if (true || pages.length > 0) {
 
     return (
       <div>
         <Page>
           <div className={"BlockPage"}>
-            {pages[0].widgets.map(({name, children, ...other}) => {
+            {currentPage.widgets.map(({name, children, ...other}) => {
 
               return (
                 <Widgets Cmp={name} {...other}>{children}</Widgets>
--- src/Hooks/usePages/usePages.js
+++ src/Hooks/usePages/usePages.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import UrlPattern from 'url-pattern';
 
 
 const usePages = () => {
@@ -38,13 +39,56 @@ const usePages = () => {
 
       });
 
+      setLoading(false);
       setPages(newPages);
 
     });
 
   }, []);
 
-  return {pages, loading, error}
+  // Todo Revisit this as I don't like the way it works.
+  const getCurrentPage = () => {
+
+    let currentUrl = "/";
+    const hashSplit = window.location.href.split('#');
+
+    if (hashSplit.length > 1) {
+      currentUrl += hashSplit[1];
+    }
+
+    const page = pages.find(({location}) => {
+
+      const pagePattern = new UrlPattern(location);
+      return pagePattern.match(currentUrl) !== null;
+
+    });
+
+    const resultFromMatch = new UrlPattern(page.location).match(currentUrl);
+
+    for (const key in resultFromMatch) {
+
+      page.widgets = page.widgets.map((widget) => {
+
+        console.log("Widget", widget);
+
+        if (widget.hasOwnProperty('_' + key)) {
+
+          delete widget['_' + key];
+          widget[key] = resultFromMatch[key];
+
+        }
+
+        return widget;
+
+      })
+
+    }
+
+    return page;
+
+  };
+
+  return {pages, loading, error, getCurrentPage}
 
 };
 
