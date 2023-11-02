cb1754cb88d625ecd511fa856b58afad311ed75c
Corofides <J.l.lendrem@smoothlines.net>
2023-11-02 12:42:33 +0000

    Move some styling into glamor and use brand Content changes.

--- src/SiteFooter/SiteFooter.js
+++ src/SiteFooter/SiteFooter.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import useSettings from '../Hooks/useSettings';
 import './SiteFooter.scss';
+import { css } from 'glamor';
 
 export default () => {
 
   const {settings} = useSettings();
 
+  const footerRule = css({
+    "padding": "16px",
+    "background-color": settings['dark-bg'],
+    "color": settings['brand-secondary-text'],
+    "width": "100%",
+    "position": "fixed",
+    "bottom": 0,
+    "box-sizing": "border-box",
+  });
+
+  const contentRule = css({
+    "max-width": "1920px",
+    "width": "100%",
+    "margin": "0 auto",
+    "display": "flex",
+    "justify-content": "space-between",
+  });
+
+  const textRule = css({
+    "text-decoration": "none",
+    "color": settings['brand-secondary-text'],
+  });
+
   return (
-    <footer className={"Footer"}>
-      <div className={"Content"}>
-        <a href={"#"}>{settings.site_owner}</a>
+    <footer {...footerRule} className={"Footer"}>
+      <div {...contentRule} className={"Content"}>
+        <a {...textRule} href={"#"}>{settings.site_owner}</a>
+        <div {...textRule}>
+          Powered by <a href={"https://github.com/Corofides/Shimmer"} {...textRule}>Shimmer</a>
+        </div>
       </div>
     </footer>
   )
--- src/SiteHeader/SiteHeader.js
+++ src/SiteHeader/SiteHeader.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import useSettings from '../Hooks/useSettings';
 import './SiteHeader.scss';
+import { css } from 'glamor';
 
 export default () => {
 
   const {settings} = useSettings();
 
+  const headerRule = css({
+    "background-color": settings['brand-primary-bg'],
+    "padding": "16px",
+    "width": "100%",
+    "box-sizing": "border-box",
+  });
+
+  const contentRule = css({
+    "display": "flex",
+    "align-items": "baseline",
+    "justify-content": "space-between",
+    "width": "100%",
+    "max-width": "1920px",
+    "margin": "0 auto",
+    "color": settings['brand-primary-text'],
+    "text-decoration": "none",
+  });
+
+  const siteNameRule = css({
+    "color": settings['brand-primary-text'],
+    "text-decoration": "none",
+    "margin": 0,
+  });
+
   return (
-    <header className={"Header"}>
-      <div className={"Content"}>
-        <h1 className={"SiteName"}>
-          <a href={"/Shimmer"}>
+    <header {...headerRule} className={"Header"}>
+      <div {...contentRule} className={"Content"}>
+        <h1 {...siteNameRule} className={"SiteName"}>
+          <a {...siteNameRule} href={"/Shimmer"}>
             {settings.site_name}
           </a>
         </h1>
@@ -20,3 +45,4 @@ export default () => {
   )
 
 }
+
