2634d084f61207569a05040264068e4dc4989b4d
Corofides <J.l.lendrem@smoothlines.net>
2023-11-03 12:39:11 +0000

    Add a loading state to the Post Preview component and move view logic into child components for greater clarity.

--- src/Widgets/PostPreview/PostPreview.js
+++ src/Widgets/PostPreview/PostPreview.js
@@ -2,71 +2,29 @@ import React from 'react';
 import {DateTime} from 'luxon';
 import './PostPreview.scss';
 import usePost from '../../Hooks/usePosts';
-import useSettings from '../../Hooks/useSettings';
-import { css } from 'glamor';
+import Loading from './Views/Loading'
+import Loaded from './Views/Loaded';
 
 
 export default ({onClick = (id) => {}, id, ...props}) => {
 
   const {getPost, loading} = usePost();
-  const {settings} = useSettings();
-
-
-  console.log("Id", id);
 
   if (loading) {
     return (
-      <div>
-        Loading
-      </div>
-    )
+      <Loading />
+    );
+
   }
 
-  const postPreviewRule = css({
-    "background-color": settings['brand-secondary-bg'],
-    "cursor": "pointer",
-    "display": "flex",
-    "flex-direction": "row",
-    "min-height": "150px",
-  });
-
-  const imageRule = css({
-    "background-color": settings['dark-bg'],
-    "position": "relative",
-    "min-width": "50%",
-  });
-
-  const subTextRule = css({
-    "color": settings["brand-secondary-text"],
-    "font-size": "14px;"
-  });
-
-  const titleRule = css({
-    "color": settings['brand-secondary-text'],
-    "margin": "auto 0 0 0",
-    "font-size": "16px",
-  });
-
-  const authorRule = css({
-    "margin-bottom": 0,
-    "margin-top": 0,
-  });
-
   const {name, author, date_published, image} = getPost(id);
-
-  console.log("PostImage", image);
-
   const dateTime = DateTime.fromISO(date_published);
+  const published = dateTime.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
 
   return (
-    <a href={"/Shimmer#posts/" + id} {...props} {...postPreviewRule} onClick={() => {onClick(id)}} className={"PostPreview"}>
-      {image ? <div {...imageRule} className={"PostPreview__Image"} /> : null}
-      <div className={"PostPreview__Info"}>
-        <span {...subTextRule} className={"PostPreview__Published"}>{dateTime.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}</span>
-        <h2 {...titleRule} className={"PostPreview__Title"}>{name}</h2>
-        <h3 {...subTextRule} {...authorRule} className={"PostPreview__Author"}>By {author}</h3>
-      </div>
-    </a>
+    <Loaded name={name} author={author} published={published} image={image} {...props} />
   )
+
+
 
 }
