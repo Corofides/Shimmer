073d4d09301e9c4ad859153ca90ed173c0cf9a20
Corofides <J.l.lendrem@smoothlines.net>
2023-11-06 12:31:02 +0000

    Continue implementing cache.

--- src/Hooks/usePosts/usePosts.js
+++ src/Hooks/usePosts/usePosts.js
@@ -1,24 +1,31 @@
 import React, {useState, useEffect} from 'react';
+import useFetch from '../useFetch';
 
 const usePosts = () => {
+
+  const {result: cachedPosts, loading: cachedLoading} = useFetch('/Shimmer/posts/posts.json');
+
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
 
-    fetch('/Shimmer/posts/posts.json').then(response => response.json()).then((result) => {
+    console.log("GetCached", cachedLoading, cachedPosts);
 
-      if (result.length > 0) {
-        result[0].active = true;
-      }
+    if (!cachedLoading) {
 
+      console.log("CachedPosts", cachedPosts);
+      setPosts(JSON.parse(cachedPosts));
       setLoading(false);
-      setPosts(result);
+      return;
 
-    });
+    }
 
-  }, []);
+    setPosts([]);
+    setLoading(true);
+
+  }, [cachedPosts, cachedLoading]);
 
   const getPost = (id) => {
     return posts.find((post) => {return post.id === id});
